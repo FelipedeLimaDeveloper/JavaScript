5756e1c4de1482b8402cb7405535df9f88eaa62a
Felipe de Lima <felipedelima.developer@gmail.com>
2020-06-01 20:58:15 -0300

    add challenge, Time Conversion

---
+++ HackerRank/Time Conversion.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', (_) => {
+  inputString = inputString
+    .trim()
+    .split('\n')
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the timeConversion function below.
+ */
+function timeConversion(s) {
+  let timeParam = s.slice(-2);
+  let hour = parseInt(s.slice(0, 2));
+  let newHour;
+  if (timeParam === 'AM') {
+    if (hour === 12) {
+      newHour = '00';
+      return newHour + s.slice(2, -2);
+    }
+    return s.slice(0, -2);
+  } else {
+    if (hour === 12) {
+      return hour + s.slice(2, -2);
+    }
+    return hour + 12 + s.slice(2, -2);
+  }
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const s = readLine();
+
+  let result = timeConversion(s);
+
+  ws.write(result + '\n');
+
+  ws.end();
+}
